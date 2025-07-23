18bd311015fe0b5dda46c5de4e45327956edf056
Arslanex <enes.career@gmail.com>
2025-07-23 17:41:50 +0300

    feat(utils): context manager fonksiyonları

--- miniflow/utils/__init__.py
+++ miniflow/utils/__init__.py
@@ -1,8 +1,10 @@
 from .miniflow_logger import setup_logging
-from .utility_functions import create_script, delete_script
+from .utility_functions import create_script, delete_script, extract_dynamic_node_params, split_variable_refrence
 
 __all__ = [
     "setup_logging",
     "create_script",
-    "delete_script"
+    "delete_script",
+    "extract_dynamic_node_params",
+    "split_variable_refrence"
 ]
--- miniflow/utils/utility_functions.py
+++ miniflow/utils/utility_functions.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 def create_script(scripts_dir: str, script_name:str, script_extension: str, script_content: str):
     # Script dosyasını script_name ile scripts klasörüne kaydet
@@ -19,3 +20,22 @@ def delete_script(scripts_dir: str, script_name: str):
         os.remove("demofile.txt")
     else:
         print("The file does not exist")
+
+def extract_dynamic_node_params(node_params):
+    pattern = r"\{\{(.*?)\}\}"
+    extract_dynamic_node_params = {}
+
+    for key, value in node_params.items():
+        if isinstance(value, str):
+            match = re.search(pattern, value)
+            if match:
+                extract_dynamic_node_params[key] = match.group(1).strip()
+    return extract_dynamic_node_params
+
+def split_variable_refrence(variable_refrence):
+    variable_parts = variable_refrence.strip().split('.')
+    if len(variable_parts) == 2:
+        variable_parts[0], variable_parts[1]
+    
+    raise ValueError(f"Invalid variable reference: {variable_refrence}")
+    
