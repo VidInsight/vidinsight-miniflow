6c6383bacff8da400f39054d36147d53d2640a39
Enes <enesars716@gmail.com>
2025-08-10 02:45:27 +0300

    feat(api): miniflow app dosyaları eklendi

---
+++ miniflow/app/config/middleware.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+
+def setup_middleware(app: FastAPI):
+    """
+    Uygulama için middleware'ları ayarla
+    """
+    # Burada ek middleware'lar eklenebilir
+    pass
---
+++ miniflow/app/utils/__init__.py
@@ -0,0 +1,12 @@
+"""
+Utils Module
+
+Utility functions and helpers for the Miniflow application
+"""
+
+# Currently no utility modules are defined
+# This file serves as a placeholder for future utility functions
+
+__all__ = [
+    # Utility functions will be added here as they are developed
+]
